b939006641124f64036b632d9e4bcc89fd892f0d
Remmy002 <remmymoreiraquispe@gmail.com>
2026-06-20 21:58:17 -0400

    GL10-Act4: TareaRequest y TareaResource

---
+++ app/Http/Resources/TareaResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TareaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->id,
+            'titulo'      => $this->titulo,
+            'descripcion' => $this->descripcion,
+            'completada'  => $this->completada,
+            'vence_el'    => $this->vence_el?->toDateString(),
+            'creada_el'   => $this->created_at->toIso8601String(),
+        ];
+    }
+}
