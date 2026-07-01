f5dbb616ea19f7b06be183941359e0377599c545
Remmy002 <remmymoreiraquispe@gmail.com>
2026-06-30 22:07:35 -0400

    GL11-Act3-4: AuthController y rutas protegidas con auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TareaController;
 
-Route::apiResource('/tareas', TareaController::class);
+// Rutas publicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Rutas protegidas (requieren token valido)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/tareas', TareaController::class);
+});
